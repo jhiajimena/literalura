e57e0b106e4da13e0f7231331c23bc3c18b81110
Pamela Lescano <jhiajimena@gmail.com>
2025-01-09 21:47:19 -0500

    subiendo proyecto final

---
+++ literalura/src/main/java/com/alura/literalura/LiteraluraApplication.java
@@ -0,0 +1,31 @@
+package com.alura.literalura;
+
+import com.alura.literalura.principal.Menu;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class LiteraluraApplication implements CommandLineRunner {
+
+	@Autowired
+	private final Menu menu;
+
+	public LiteraluraApplication(Menu menu) {
+		this.menu = menu;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraluraApplication.class, args);
+
+	}
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		menu.mostrarMenu();
+
+	}
+}
